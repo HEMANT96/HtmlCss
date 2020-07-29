9fbdc26564d3b511f40ccaa8e93a189fc73d95d9
SunzoneTech <hemu9675132096@gmail.com>
2020-07-29 21:04:36 +0530

    commit

---
+++ WEBAPP/myscript.js
@@ -0,0 +1,8 @@
+alert("Hello")
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
